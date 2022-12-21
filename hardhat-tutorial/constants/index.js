10b4d9edb366c655b55b0b1fb1de7530c7329c56
wildcat1853 <talkotick@gmail.com>
2022-12-21 13:21:51 +0100

    updated NFT contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,7 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x68B2eaa399Dc20aEa90d5767a2833Fd52de3309b";
+const WHITELIST_CONTRACT_ADDRESS = "0xc27A8D1ebd41197D11D32cf83B53Ef5be3977f3D";
+
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-chi-three-69.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
